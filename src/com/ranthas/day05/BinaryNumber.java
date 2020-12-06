e1b5b68c305a8cd7d810ae9c842b1c8b4122b084
ranthas90 <rubenreal1990@gmail.com>
2020-12-06 11:44:23 +0000

    solved 5th day

---
+++ src/com/ranthas/day05/BinaryNumber.java
@@ -0,0 +1,45 @@
+package com.ranthas.day05;
+
+public class BinaryNumber {
+
+    private final String row;
+    private final String column;
+
+    public BinaryNumber(String number) {
+        this.row = number.substring(0, 7);
+        this.column = number.substring(7);
+    }
+
+    public long getUniqueID() {
+        return getDecimalValue(row) * 8L + getDecimalValue(column);
+    }
+
+    private long getDecimalValue(String number) {
+
+        long decimalValue = 0L;
+
+        for (int i = 0; i < number.length(); i++) {
+
+            long exponent = number.length() - 1 - i;
+            long pow = (long) Math.pow(2,exponent);
+
+
+            decimalValue = decimalValue + (getBinaryValue(number.charAt(i)) * pow);
+        }
+
+        return decimalValue;
+    }
+
+    private long getBinaryValue(char character) {
+
+        if (character =='F' || character == 'L') {
+            return 0L;
+        }
+
+        if (character == 'B' || character == 'R') {
+            return 1L;
+        }
+
+        throw new IllegalArgumentException("Character cannot be different from F or B => " + character);
+    }
+}
---
+++ src/com/ranthas/day05/part02/DayFive.java
@@ -0,0 +1,38 @@
+package com.ranthas.day05.part02;
+
+import com.ranthas.day05.BinaryNumber;
+import com.ranthas.util.FileService;
+import com.ranthas.util.FileServiceImpl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class DayFive {
+
+    private static final String DATA_INPUT_FILE = "src/com/ranthas/day05/data-input.aoc";
+
+    public static void main(String[] args) {
+
+        FileService fileService = new FileServiceImpl();
+        final List<Long> collect =
+                fileService
+                .readAllLines(DATA_INPUT_FILE)
+                .stream()
+                .map(line -> new BinaryNumber(line).getUniqueID())
+                .sorted()
+                .collect(Collectors.toList());
+
+        for (int i = 1; i < collect.size() - 1; i++) {
+
+            long currentNumber = collect.get(i);
+            long previousNumber = collect.get(i - 1);
+
+            if (previousNumber + 1 == currentNumber - 1) {
+                System.out.println("The solution is " + (currentNumber - 1));
+                System.exit(0);
+            }
+        }
+
+        System.err.println("No solution found!!");
+    }
+}
